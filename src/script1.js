2719906061e5edc066db9509ec892be1935eba12
SREEVINAYAKG <srivinayakg2005@gmail.com>
2025-07-29 22:00:35 +0530

    modified styles of donor form and made it responsive

--- src/script1.js
+++ src/script1.js
@@ -5,7 +5,9 @@ const app = express();
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(express.static('assets'));
+// app.use(express.static('assets'));
+app.use(express.static(path.join(__dirname, '..', 'assets')));
+app.set('views', path.join(__dirname, '..', 'views'));
 
 app.get("/",(req,res)=>{
     res.render("index");
@@ -19,4 +21,9 @@ app.get("/login",(req,res)=>{
 app.get("/about",(req,res)=>{
     res.render("about");
 })
-app.listen(3000);
+app.get("/food_data",(req,res)=>{
+    res.render("food_data");
+})
+app.listen(3000,() => {
+  console.log(`Server is running on http://localhost:3000`);
+});
