6d323fb4d5dac0bf7c1af9f08388d994aaf07f1d
SREEVINAYAKG <srivinayakg2005@gmail.com>
2025-08-05 05:29:41 +0530

    deleted unwanted file

--- src/script.js
+++
@@ -1,9 +0,0 @@
-// const volunteer = document.querySelector('.volunteer');
-// const findFood = document.querySelector('.find-food');
-// volunteer.addEventListener('click', () => {
-//     window.location.href = './login.html';
-// });
-// findFood.addEventListener('click', () => {
-//     window.location.href = './food_data.ejs';
-// });
-
