53412db53573010cce41a49a1305d287e90fcfb3
SREEVINAYAKG <srivinayakg2005@gmail.com>
2025-08-04 19:12:41 +0530

    modified some functionalities

--- src/script1.js
+++ src/script1.js
@@ -1,6 +1,5 @@
 const express= require('express');
 const path=require('path');
-
 const app = express();
 app.set('view engine','ejs');
 app.use(express.json());
@@ -8,6 +7,35 @@ app.use(express.urlencoded({extended:false}));
 // app.use(express.static('assets'));
 app.use(express.static(path.join(__dirname, '..', 'assets')));
 app.set('views', path.join(__dirname, '..', 'views'));
+const pg = require('pg');
+
+
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "ibm_skillsbuild_proj_db",
+    password:"Vinayak@1234",
+    port: 5433,
+})
+
+db.connect()
+    .then(() => {
+        console.log("Connected to the database");
+    })
+    .catch((err) => {
+        console.error("Database connection error:", err);
+    });
+
+    const query = "SELECT * FROM food_items";
+    db.query(query)
+        .then((result) => {
+            console.log("Food items retrieved:", result.rows);
+        })
+        .catch((err) => {
+            console.error("Error retrieving food items:", err);
+        });
+
 
 app.get("/",(req,res)=>{
     res.render("index");
@@ -24,6 +52,35 @@ app.get("/about",(req,res)=>{
 app.get("/food_data",(req,res)=>{
     res.render("food_data");
 })
+app.post("/donor_dashboard", (req, res) => {
+    const { foodname, amount, address, phone_num } = req.body;
+
+    const query = "INSERT INTO food_items (food_name, quantity, address, phone_num) VALUES ($1, $2, $3, $4)";
+    const values = [foodname, amount, address, phone_num];
+    console.log("Received data:", { food_name: foodname, quantity: amount, address, phone_num });
+
+    db.query(query, values)
+        .then(() => {
+            console.log("Food item donated:", { food_name: foodname, quantity: amount, address, phone_num });
+            res.redirect("/donor_dashboard");
+        })
+        .catch((err) => {
+            console.error("Error donating food item:", err);
+            res.status(500).send("Internal Server Error");
+        });
+});
+
+const foodDataQuery = "SELECT * FROM food_items";
+app.get("/donor_dashboard", (req, res) => {
+    db.query(foodDataQuery)
+        .then((result) => {
+            res.render("donor_food_data", { foodItems: result.rows });
+        })
+        .catch((err) => {
+            console.error("Error retrieving food data:", err);
+            res.status(500).send("Internal Server Error");
+        });
+});
 app.listen(3000,() => {
   console.log(`Server is running on http://localhost:3000`);
 });
